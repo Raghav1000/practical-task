1e87f8bc580aa4681d631e2f93f2e286a32b5616
Raghav <raghlm10@gmail.com>
2025-12-06 13:42:47 +0530

    updated migration file

--- server/src/database/migrations/1765008415672-migration.ts
+++ server/src/database/migrations/1765008415672-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1765001936422 implements MigrationInterface {
-    name = 'Migration1765001936422'
+export class Migration1765008415672 implements MigrationInterface {
+    name = 'Migration1765008415672'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "posts" ("id" SERIAL NOT NULL, "title" text NOT NULL, "description" text NOT NULL, "imageURL" character varying, "likeCount" integer NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "authorId" integer NOT NULL, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
